de4125c7231bc6dc5b502de6d021cdaaea77091d
cafesmire <cafesmire@gmail.com>
2026-02-07 16:44:47 -0500

    Style mood card

--- src/components/MoodCard.jsx
+++ src/components/MoodCard.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
+
 export function MoodCard() {
-  return <div>
-    Mood
-  </div>;
+  const [currentMood, setCurrentMood] = useState(null);
+  const moods = ['😭', '😔', '😐', '🙂', '😁'];
+	return (
+		<div className="mood">
+			<h3>How Are You Feeling Today?</h3>
+			<div>
+				{moods.map((mood, idx) => {
+					return (
+						<span
+							onClick={() => setCurrentMood(idx)}
+							style={{
+								fontSize: '4rem',
+								cursor: 'pointer',
+								marginInline: '.25em',
+								border: currentMood === idx ? '1px solid white' : '',
+                borderRadius:'50%'
+							}}
+							key={idx}
+						>
+							{mood}
+						</span>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
