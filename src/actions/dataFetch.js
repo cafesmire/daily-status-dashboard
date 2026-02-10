24b7c3359d66569888ac91a3cebb84f4fae361f8
cafesmire <cafesmire@gmail.com>
2026-02-09 19:54:49 -0500

    Add .env file

--- src/actions/dataFetch.js
+++ src/actions/dataFetch.js
@@ -1,11 +1,10 @@
 export async function getQuoteOfTheDay() {
-	const url =
-		'https://quotes-api12.p.rapidapi.com/dev-jokes?category=all&subcategory=javascript';
-	const options = {
+	const url = import.meta.env.VITE_QUOTE_URL;
+		const options = {
 		method: 'GET',
 		headers: {
-			'x-rapidapi-key': 'd017988155msh9becd163b108d42p127a0fjsn4b469339dd8c',
-			'x-rapidapi-host': 'quotes-api12.p.rapidapi.com',
+			'x-rapidapi-key': import.meta.env.VITE_QUOTE_KEY,
+			'x-rapidapi-host': import.meta.env.VITE_QUOTE_HOST,
 		},
 	};
 
