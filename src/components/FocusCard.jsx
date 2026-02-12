9449d5884fc447aaeabf471519ff36b456e0605f
cafesmire <cafesmire@gmail.com>
2026-02-12 16:24:25 -0500

    Add saving/editing focus card

--- src/components/FocusCard.jsx
+++ src/components/FocusCard.jsx
@@ -2,18 +2,34 @@ import {useState} from 'react';
 
 export function FocusCard() {
 	const [focusState, setFocusState] = useState("");
+	const [savedFocus, setSavedFocus] = useState(false);
 
+	const changeFocus = () => {
+		setFocusState("");
+		setSavedFocus(false);
+	}
 	return (
 		<div className='focus'>
 			<h3>Today I Need To Focus On...</h3>
-			<textarea
+			{
+				savedFocus ?
+				<p>{focusState}</p>
+				:
+				<textarea
 				className='border p-2 rounded-xl min-h-1/2'
         onChange={(e) => setFocusState(e.target.value)}
 				name='focusText'
         defaultValue={focusState}
         placeholder='I want to accomplish...'
 			></textarea>
-			<button>Save</button>
+			
+			}
+			{
+			savedFocus?
+			<button onClick={changeFocus}>Change Focus</button>
+			:
+			<button onClick={() => setSavedFocus(true)}>Save</button>
+			}
 		</div>
 	);
 }
--- src/components/MoodCard.jsx
+++ src/components/MoodCard.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 export function MoodCard() {
   const [currentMood, setCurrentMood] = useState(null);
-  const moods = ['😭', '😔', '😐', '🙂', '😁'];
-	return (
-		<div className="mood">
-			<h3>How Are You Feeling Today?</h3>
-			<div className='md:flex'>
-				{moods.map((mood, idx) => {
-					return (
-						<p
-							onClick={() => setCurrentMood(idx)}
-							style={{
-								fontSize: '4rem',
-								cursor: 'pointer',
-								border: currentMood === idx ? '1px solid white' : '',
-								marginInline:'auto',
-								borderRadius: '50%',
-								padding:'1rem'
-							}}
-							key={idx}
-						>
-							{mood}
-						</p>
-					);
-				})}
-			</div>
-		</div>
-	);
+  const moods = ["😭", "😔", "😐", "🙂", "😁"];
+  return (
+    <div className="mood">
+      <h3>How Are You Feeling Today?</h3>
+      <div className="md:flex justify-between">
+        {moods.map((mood, idx) => {
+          return (
+            <p
+							className="cursor-pointer text-[4rem] rounded-full px-4"
+              onClick={() => setCurrentMood(idx)}
+              style={{ border: currentMood === idx ? "1px solid #ddd" : ""}}
+              key={idx}
+            >
+              {mood}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
